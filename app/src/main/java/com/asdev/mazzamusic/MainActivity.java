c94e26cd9357bb161094a53cce05bb3ee1e465c9
aditya <x3.aditya@gmail.com>
2019-03-19 01:35:11 +0530

    Aditya's First Commit

--- app/src/main/java/com/asdev/mazzamusic/MainActivity.java
+++ app/src/main/java/com/asdev/mazzamusic/MainActivity.java
@@ -2,6 +2,7 @@ package com.asdev.mazzamusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Commit", Toast.LENGTH_SHORT).show();
     }
 }
