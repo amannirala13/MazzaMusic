563c7ef94146a047ff865c951fa71a14d91cb1bd
amannirala13 <amansofttechinfo@gmail.com>
2019-03-20 14:58:41 +0530

    Change 1

--- app/src/main/java/com/asdev/mazzamusic/MainActivity.java
+++ app/src/main/java/com/asdev/mazzamusic/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Commit", Toast.LENGTH_SHORT).show();
+        //Change 1
     }
 }
